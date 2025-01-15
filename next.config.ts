013f5f4a26be893f65809fcc2bca8bb4b7f35ad1
trace-panic <patrickscript@proton.me>
2025-01-15 09:44:12 +0300

    Setup netra

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import nextra from "nextra";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withNextra = nextra({
+  defaultShowCopyCode: true,
+  // ... Other Nextra config options
+});
 
-export default nextConfig;
+// You can include other Next.js configuration options here, in addition to Nextra settings:
+export default withNextra({
+  // ... Other Next.js config options
+});
