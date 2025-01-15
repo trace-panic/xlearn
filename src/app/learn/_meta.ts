ea24f675b1b0468a6cb3ee955519e718da65d528
trace-panic <patrickscript@proton.me>
2025-01-15 15:15:04 +0300

    Update _meta files

--- src/app/learn/_meta.ts
+++ src/app/learn/_meta.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   index: "",
-  learn: "",
+  bash: "",
 };
--- src/app/learn/bash/_meta.ts
+++ src/app/learn/bash/_meta.ts
@@ -1,5 +1,4 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  index: "",
-  learn: "",
+  index: "Introduction",
 };
