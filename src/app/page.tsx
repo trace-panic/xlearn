8251262940e4f20b7a62dfb69e176d4ac45fade2
trace-panic <patrickscript@proton.me>
2025-01-15 14:23:10 +0300

    Add landing page

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+import Hero from "./hero";
+
+export const metadata: Metadata = {
+  title: "XLearn - Learn Programming & Frameworks",
+};
+
+export default function Home() {
+  return <Hero />;
+}
